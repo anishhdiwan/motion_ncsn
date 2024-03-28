3512e0065f075a78ff5395f393a1b21df9435a2c
anishdiwan <anishdiwan@diwan-ubuntu20.04>
2024-03-28 15:16:22 +0100

    Clean up anneal_runner

--- runners/anneal_runner.py
+++ runners/anneal_runner.py
@@ -39,6 +39,7 @@ __all__ = ['AnnealRunner']
 from sklearn import datasets
 import matplotlib.pyplot as plt
 
+
 class SwissRollDataset(torch.utils.data.Dataset):
     def __init__(self, n_samples=20000, state=0, norm=True):
 
@@ -226,102 +227,6 @@ class AnnealRunner():
 
             logging.info(f"Epoch {epoch} Avg Loss: {avg_loss/len(dataloader)}")
 
-    def Langevin_dynamics(self, x_mod, scorenet, n_steps=200, step_lr=0.00005):
-        images = []
-
-        labels = torch.ones(x_mod.shape[0], device=x_mod.device) * 9
-        labels = labels.long()
-
-        with torch.no_grad():
-            for _ in range(n_steps):
-                images.append(torch.clamp(x_mod, 0.0, 1.0).to('cpu'))
-                noise = torch.randn_like(x_mod) * np.sqrt(step_lr * 2)
-                grad = scorenet(x_mod, labels)
-                x_mod = x_mod + step_lr * grad + noise
-                x_mod = x_mod
-                print("modulus of grad components: mean {}, max {}".format(grad.abs().mean(), grad.abs().max()))
-
-            return images
-
-    def anneal_Langevin_dynamics(self, x_mod, scorenet, sigmas, n_steps_each=100, step_lr=0.00002):
-        images = []
-
-        with torch.no_grad():
-            for c, sigma in tqdm.tqdm(enumerate(sigmas), total=len(sigmas), desc='annealed Langevin dynamics sampling'):
-                labels = torch.ones(x_mod.shape[0], device=x_mod.device) * c
-                labels = labels.long()
-                step_size = step_lr * (sigma / sigmas[-1]) ** 2
-                for s in range(n_steps_each):
-                    images.append(torch.clamp(x_mod, 0.0, 1.0).to('cpu'))
-                    noise = torch.randn_like(x_mod) * np.sqrt(step_size * 2)
-                    grad = scorenet(x_mod, labels)
-                    x_mod = x_mod + step_size * grad + noise
-                    # print("class: {}, step_size: {}, mean {}, max {}".format(c, step_size, grad.abs().mean(),
-                    #                                                          grad.abs().max()))
-
-            return images
-
-
-    def default_test(self):
-        states = torch.load(os.path.join(self.args.log, 'checkpoint.pth'), map_location=self.config.device)
-        score = CondRefineNetDilated(self.config).to(self.config.device)
-        score = torch.nn.DataParallel(score)
-
-        score.load_state_dict(states[0])
-
-        if not os.path.exists(self.args.image_folder):
-            os.makedirs(self.args.image_folder)
-
-        sigmas = np.exp(np.linspace(np.log(self.config.model.sigma_begin), np.log(self.config.model.sigma_end),
-                                    self.config.model.L))
-
-        score.eval()
-        grid_size = 5
-
-        imgs = []
-        if self.config.data.dataset == 'MNIST':
-            samples = torch.rand(grid_size ** 2, 1, 28, 28, device=self.config.device)
-            all_samples = self.anneal_Langevin_dynamics(samples, score, sigmas, 100, 0.00002)
-
-            for i, sample in enumerate(tqdm.tqdm(all_samples, total=len(all_samples), desc='saving images')):
-                sample = sample.view(grid_size ** 2, self.config.data.channels, self.config.data.image_size,
-                                     self.config.data.image_size)
-
-                if self.config.data.logit_transform:
-                    sample = torch.sigmoid(sample)
-
-                image_grid = make_grid(sample, nrow=grid_size)
-                if i % 10 == 0:
-                    im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
-                    imgs.append(im)
-
-                save_image(image_grid, os.path.join(self.args.image_folder, 'image_{}.png'.format(i)))
-                torch.save(sample, os.path.join(self.args.image_folder, 'image_raw_{}.pth'.format(i)))
-
-
-        else:
-            samples = torch.rand(grid_size ** 2, 3, 32, 32, device=self.config.device)
-
-            all_samples = self.anneal_Langevin_dynamics(samples, score, sigmas, 100, 0.00002)
-
-            for i, sample in enumerate(tqdm.tqdm(all_samples, total=len(all_samples), desc='saving images')):
-                sample = sample.view(grid_size ** 2, self.config.data.channels, self.config.data.image_size,
-                                     self.config.data.image_size)
-
-                if self.config.data.logit_transform:
-                    sample = torch.sigmoid(sample)
-
-                image_grid = make_grid(sample, nrow=grid_size)
-                if i % 10 == 0:
-                    im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
-                    imgs.append(im)
-
-                save_image(image_grid, os.path.join(self.args.image_folder, 'image_{}.png'.format(i)), nrow=10)
-                torch.save(sample, os.path.join(self.args.image_folder, 'image_raw_{}.pth'.format(i)))
-
-        imgs[0].save(os.path.join(self.args.image_folder, "movie.gif"), save_all=True, append_images=imgs[1:], duration=1, loop=0)
-
-
 
     def test(self):
         device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -390,137 +295,39 @@ class AnnealRunner():
             plt.show()
 
 
-            # samples = torch.rand(grid_size ** 2, 1, 28, 28, device=self.config.device)
-            # all_samples = self.anneal_Langevin_dynamics(samples, score, sigmas, 100, 0.00002)
-
-            # for i, sample in enumerate(tqdm.tqdm(all_samples, total=len(all_samples), desc='saving images')):
-            #     sample = sample.view(grid_size ** 2, self.config.data.channels, self.config.data.image_size,
-            #                          self.config.data.image_size)
-
-            #     if self.config.data.logit_transform:
-            #         sample = torch.sigmoid(sample)
-
-            #     image_grid = make_grid(sample, nrow=grid_size)
-            #     if i % 10 == 0:
-            #         im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
-            #         imgs.append(im)
-
-            #     save_image(image_grid, os.path.join(self.args.image_folder, 'image_{}.png'.format(i)))
-            #     torch.save(sample, os.path.join(self.args.image_folder, 'image_raw_{}.pth'.format(i)))
 
 
+    # def Langevin_dynamics(self, x_mod, scorenet, n_steps=200, step_lr=0.00005):
+    #     images = []
 
-    def anneal_Langevin_dynamics_inpainting(self, x_mod, refer_image, scorenet, sigmas, n_steps_each=100,
-                                            step_lr=0.000008):
-        images = []
+    #     labels = torch.ones(x_mod.shape[0], device=x_mod.device) * 9
+    #     labels = labels.long()
 
-        refer_image = refer_image.unsqueeze(1).expand(-1, x_mod.shape[1], -1, -1, -1)
-        refer_image = refer_image.contiguous().view(-1, 3, 32, 32)
-        x_mod = x_mod.view(-1, 3, 32 ,32)
-        half_refer_image = refer_image[..., :16]
-        with torch.no_grad():
-            for c, sigma in tqdm.tqdm(enumerate(sigmas), total=len(sigmas), desc="annealed Langevin dynamics sampling"):
-                labels = torch.ones(x_mod.shape[0], device=x_mod.device) * c
-                labels = labels.long()
-                step_size = step_lr * (sigma / sigmas[-1]) ** 2
+    #     with torch.no_grad():
+    #         for _ in range(n_steps):
+    #             images.append(torch.clamp(x_mod, 0.0, 1.0).to('cpu'))
+    #             noise = torch.randn_like(x_mod) * np.sqrt(step_lr * 2)
+    #             grad = scorenet(x_mod, labels)
+    #             x_mod = x_mod + step_lr * grad + noise
+    #             x_mod = x_mod
+    #             print("modulus of grad components: mean {}, max {}".format(grad.abs().mean(), grad.abs().max()))
 
-                corrupted_half_image = half_refer_image + torch.randn_like(half_refer_image) * sigma
-                x_mod[:, :, :, :16] = corrupted_half_image
-                for s in range(n_steps_each):
-                    images.append(torch.clamp(x_mod, 0.0, 1.0).to('cpu'))
-                    noise = torch.randn_like(x_mod) * np.sqrt(step_size * 2)
-                    grad = scorenet(x_mod, labels)
-                    x_mod = x_mod + step_size * grad + noise
-                    x_mod[:, :, :, :16] = corrupted_half_image
-                    # print("class: {}, step_size: {}, mean {}, max {}".format(c, step_size, grad.abs().mean(),
-                    #                                                          grad.abs().max()))
+    #         return images
 
-            return images
+    # def anneal_Langevin_dynamics(self, x_mod, scorenet, sigmas, n_steps_each=100, step_lr=0.00002):
+    #     images = []
 
-    def test_inpainting(self):
-        states = torch.load(os.path.join(self.args.log, 'checkpoint.pth'), map_location=self.config.device)
-        score = CondRefineNetDilated(self.config).to(self.config.device)
-        score = torch.nn.DataParallel(score)
+    #     with torch.no_grad():
+    #         for c, sigma in tqdm.tqdm(enumerate(sigmas), total=len(sigmas), desc='annealed Langevin dynamics sampling'):
+    #             labels = torch.ones(x_mod.shape[0], device=x_mod.device) * c
+    #             labels = labels.long()
+    #             step_size = step_lr * (sigma / sigmas[-1]) ** 2
+    #             for s in range(n_steps_each):
+    #                 images.append(torch.clamp(x_mod, 0.0, 1.0).to('cpu'))
+    #                 noise = torch.randn_like(x_mod) * np.sqrt(step_size * 2)
+    #                 grad = scorenet(x_mod, labels)
+    #                 x_mod = x_mod + step_size * grad + noise
+    #                 # print("class: {}, step_size: {}, mean {}, max {}".format(c, step_size, grad.abs().mean(),
+    #                 #                                                          grad.abs().max()))
 
-        score.load_state_dict(states[0])
-
-        if not os.path.exists(self.args.image_folder):
-            os.makedirs(self.args.image_folder)
-
-        sigmas = np.exp(np.linspace(np.log(self.config.model.sigma_begin), np.log(self.config.model.sigma_end),
-                                    self.config.model.L))
-        score.eval()
-
-        imgs = []
-        if self.config.data.dataset == 'CELEBA':
-            dataset = CelebA(root=os.path.join(self.args.run, 'datasets', 'celeba'), split='test',
-                             transform=transforms.Compose([
-                                 transforms.CenterCrop(140),
-                                 transforms.Resize(self.config.data.image_size),
-                                 transforms.ToTensor(),
-                             ]), download=True)
-
-            dataloader = DataLoader(dataset, batch_size=20, shuffle=True, num_workers=4)
-            refer_image, _ = next(iter(dataloader))
-
-            samples = torch.rand(20, 20, 3, self.config.data.image_size, self.config.data.image_size,
-                                 device=self.config.device)
-
-            all_samples = self.anneal_Langevin_dynamics_inpainting(samples, refer_image, score, sigmas, 100, 0.00002)
-            torch.save(refer_image, os.path.join(self.args.image_folder, 'refer_image.pth'))
-
-            for i, sample in enumerate(tqdm.tqdm(all_samples)):
-                sample = sample.view(400, self.config.data.channels, self.config.data.image_size,
-                                     self.config.data.image_size)
-
-                if self.config.data.logit_transform:
-                    sample = torch.sigmoid(sample)
-
-                image_grid = make_grid(sample, nrow=20)
-                if i % 10 == 0:
-                    im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
-                    imgs.append(im)
-
-                save_image(image_grid, os.path.join(self.args.image_folder, 'image_completion_{}.png'.format(i)))
-                torch.save(sample, os.path.join(self.args.image_folder, 'image_completion_raw_{}.pth'.format(i)))
-
-        else:
-            transform = transforms.Compose([
-                transforms.Resize(self.config.data.image_size),
-                transforms.ToTensor()
-            ])
-
-            if self.config.data.dataset == 'CIFAR10':
-                dataset = CIFAR10(os.path.join(self.args.run, 'datasets', 'cifar10'), train=True, download=True,
-                                  transform=transform)
-            elif self.config.data.dataset == 'SVHN':
-                dataset = SVHN(os.path.join(self.args.run, 'datasets', 'svhn'), split='train', download=True,
-                               transform=transform)
-
-            dataloader = DataLoader(dataset, batch_size=20, shuffle=True, num_workers=4)
-            data_iter = iter(dataloader)
-            refer_image, _ = next(data_iter)
-
-            torch.save(refer_image, os.path.join(self.args.image_folder, 'refer_image.pth'))
-            samples = torch.rand(20, 20, self.config.data.channels, self.config.data.image_size,
-                                 self.config.data.image_size).to(self.config.device)
-
-            all_samples = self.anneal_Langevin_dynamics_inpainting(samples, refer_image, score, sigmas, 100, 0.00002)
-
-            for i, sample in enumerate(tqdm.tqdm(all_samples)):
-                sample = sample.view(400, self.config.data.channels, self.config.data.image_size,
-                                     self.config.data.image_size)
-
-                if self.config.data.logit_transform:
-                    sample = torch.sigmoid(sample)
-
-                image_grid = make_grid(sample, nrow=20)
-                if i % 10 == 0:
-                    im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
-                    imgs.append(im)
-
-                save_image(image_grid, os.path.join(self.args.image_folder, 'image_completion_{}.png'.format(i)))
-                torch.save(sample, os.path.join(self.args.image_folder, 'image_completion_raw_{}.pth'.format(i)))
-
-
-        imgs[0].save(os.path.join(self.args.image_folder, "movie.gif"), save_all=True, append_images=imgs[1:], duration=1, loop=0)
+    #         return images
