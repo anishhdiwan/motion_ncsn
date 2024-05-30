30fdcd880136c40a1dd59260f7d69a722dc7ad76
anishdiwan <anishdiwan@diwan-ubuntu20.04>
2024-05-30 19:53:29 +0200

    Add energy curve plotting and plot visualisation from main file

--- losses/dsm.py
+++ losses/dsm.py
@@ -1,7 +1,8 @@
 import torch
 import torch.autograd as autograd
 import torch.nn.functional as F
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 def dsm(energy_net, samples, sigma=1):
     samples.requires_grad_(True)
@@ -77,13 +78,18 @@ def anneal_dsm_score_estimation(network, samples, labels, sigmas, anneal_power=2
     scores = scores.view(scores.shape[0], -1)
 
     if REGULARISE_ENERGY:
-        # Regularise to get low energy values. Ensures consistency between the energy functions learnt for different noise levels
         loss = 1 / 2. * ((scores - target) ** 2).sum(dim=-1) * used_sigmas.squeeze() ** anneal_power + torch.linalg.norm(energy, dim=1, ord=1)
     
     else:
         loss = 1 / 2. * ((scores - target) ** 2).sum(dim=-1) * used_sigmas.squeeze() ** anneal_power
 
-    return loss.mean(dim=0)
+    if grad:
+        return loss.mean(dim=0)
+    else:
+        test_set_energies = compute_anneal_dsm_energies(network, samples, sigmas, perturbation="gaussian")
+        return loss.mean(dim=0), test_set_energies
+
+
 
 
 
@@ -124,4 +130,57 @@ def anneal_dsm_energy_estimation(network, samples, labels, sigmas, anneal_power=
 
     loss = 1 / 2. * ((energy - target) ** 2).sum(dim=-1) * used_sigmas.squeeze() ** anneal_power
 
-    return loss.mean(dim=0)
+    if grad:
+        return loss.mean(dim=0)
+    else:
+        test_set_energies = compute_anneal_dsm_energies(network, samples, sigmas, perturbation=DIST_KERNEL)
+        return loss.mean(dim=0), test_set_energies
+
+
+def compute_anneal_dsm_energies(network, samples, sigmas, perturbation):
+    """Compute the average energy of the samples for multiple label values
+    """
+    labels_to_evaluate = [0,3,5,7,9]
+    avg_energy = {}
+    for noise_level in labels_to_evaluate:
+        labels = torch.full((samples.shape[0],), noise_level, device=samples.device)
+        # used_sigmas = sigmas[labels].view(samples.shape[0], *([1] * len(samples.shape[1:])))
+
+        # if perturbation == "gaussian":
+        #     perturbed_samples = samples + torch.randn_like(samples) * used_sigmas
+        # elif perturbation == "uniform":
+        #     perturbed_samples = samples + ((1 - 2*torch.rand(samples.shape, device=samples.device)) * used_sigmas)
+        
+        # perturbed_samples = perturbed_samples.to(torch.float)
+        energy = network(samples.to(torch.float), labels)
+        avg_energy[f"sigma_level_{noise_level}"] = energy.squeeze().mean()
+
+    return avg_energy
+
+def plot_energy_curve(network, samples):
+    """Plot a curve with the average energy of a set of samples on the y-axis and the distance of the samples from the demo dataset on the x-axis
+    """
+    # Absolute values of the range [-r, r] of a uniform distribution from which demo data is perturbed
+    demo_sample_max_distances = np.linspace(1, 100, 100)
+    labels_to_evaluate = [0,3,5,7,9]
+
+    for noise_level in labels_to_evaluate:
+        labels = torch.full((samples.shape[0],), noise_level, device=samples.device)
+        avg_energy = np.zeros_like(demo_sample_max_distances)
+
+        for idx, max_dist in enumerate(demo_sample_max_distances):
+            perturbed_samples = samples + (max_dist -2*max_dist*torch.rand(samples.shape, device=samples.device))
+            energy = network(perturbed_samples.to(torch.float), labels)
+            avg_energy[idx] = energy.squeeze().mean()
+
+        plt.figure(figsize=(8, 6))
+        plt.plot(demo_sample_max_distances, avg_energy)
+        plt.xlabel("max perturbation r (sample = sample + unif[-r,r])")
+        plt.ylabel("avg energy")
+        plt.title(f"Avg energy vs distance from demo data at sigma_level_{noise_level}")
+        plt.show()
+        
+
+
+
+
--- runners/anneal_runner.py
+++ runners/anneal_runner.py
@@ -229,6 +229,9 @@ class AnnealRunner():
                 # logging.info("step: {}, loss: {}".format(step, loss.item()))
 
                 if step >= self.config.training.n_iters:
+                    # Temporary testing
+                    self.plot_energy_landscape(test_loader, score)
+                    # Quit
                     return 0
 
                 if step % 100 == 0:
@@ -252,10 +255,12 @@ class AnnealRunner():
 
                     # with torch.no_grad():
                     # Instead of setting no_grad, explicitly compute scores as gradients without adding to the graph
-                    test_dsm_loss = anneal_dsm_loss(score, test_X, test_labels, sigmas,
+                    test_dsm_loss, test_energies = anneal_dsm_loss(score, test_X, test_labels, sigmas,
                                                                     self.config.training.anneal_power, grad=False)
 
                     tb_logger.add_scalar('test_dsm_loss', test_dsm_loss, global_step=step)
+                    for k, v in test_energies.items():
+                        tb_logger.add_scalar(f'demo_data_energy/{k}', v, global_step=step)
 
                 if step % self.config.training.snapshot_freq == 0:
                     states = [
@@ -272,11 +277,158 @@ class AnnealRunner():
             # logging.info(f"Epoch {epoch} Avg Loss: {avg_loss/len(dataloader)}")
 
 
+
     def visualise_energy(self):
         if self.config.data.dataset == 'Swiss-Roll':
             self.visualise_sr_energy()
         elif self.config.data.dataset == 'maze':
             self.visualise_2d_energy()
+            # self.plot_energy_landscape()
+        elif self.config.data.dataset == 'humanoid':
+            self.plot_energy_landscape()
+
+    def plot_energy_landscape(self):
+        """Plot a curve with the average energy of a set of samples on the y-axis and the distance of the samples from the demo dataset on the x-axis
+        """
+
+        if self.config.data.dataset == 'maze':
+            motion_lib = GymMotionLib(self.config.data.motion_file, self.config.model.numObsSteps, self.config.model.in_dim, episodic=False, normalize=False, test_split=True, auto_ends=False)
+            dataloader, test_loader = motion_lib.get_traj_agnostic_dataloader(batch_size=self.config.training.batch_size, shuffle=True)
+
+        if self.config.data.dataset == 'humanoid':
+            # Setting up a dict for motion lib processing
+            humanoid_cfg = OmegaConf.create()
+            humanoid_cfg.physics_engine = self.config.data.physics_engine
+            humanoid_cfg.sim = self.config.data.sim_params
+            humanoid_cfg.env = self.config.data.env_params
+            humanoid_cfg.env.numEnvs = self.config.data.numEnvs
+            humanoid_cfg.sim.use_gpu_pipeline = self.config.data.use_gpu_pipeline
+            humanoid_cfg.sim.physx.num_threads = self.config.data.num_threads
+            humanoid_cfg.sim.physx.solver_type = self.config.data.solver_type
+            humanoid_cfg.sim.physx.num_subscenes = self.config.data.num_subscenes
+            humanoid_cfg.sim.physx.use_gpu = self.config.data.use_gpu
+
+            motion_lib = HumanoidMotionLib(self.config.data.env_params.motion_file, humanoid_cfg, self.config.device)
+            dataloader = motion_lib.get_dataloader(batch_size=self.config.training.batch_size, buffer_size=self.config.training.buffer_size, shuffle=True)
+            test_loader = dataloader
+
+
+        if self.config.training.normalize_energynet_input:
+            running_mean_std = RunningMeanStd(torch.ones(self.config.model.in_dim * self.config.model.numObsSteps).shape).to(self.config.device)
+            running_mean_std_states = torch.load(self.config.inference.running_mean_std_checkpoint, map_location=self.config.device)
+            running_mean_std.load_state_dict(running_mean_std_states)
+            running_mean_std.eval()
+            
+            print(f"EnergyNet was trained using normalised inputs. Data mean {running_mean_std.running_mean} Data var {running_mean_std.running_var}")
+
+
+        device = self.config.device
+        states = torch.load(self.config.inference.eb_model_checkpoint, map_location=self.config.device)
+        network = SimpleNet(self.config).to(self.config.device)
+        network = torch.nn.DataParallel(network)
+        network.load_state_dict(states[0])
+        network.eval()
+
+        test_iter = iter(test_loader)
+        test_X = next(test_iter)
+        num_batches_to_sample = 15
+        while test_X.shape[0] < num_batches_to_sample*self.config.training.batch_size:
+            try:
+                next_test_X = next(test_iter)
+            except StopIteration:
+                test_iter = iter(test_loader)
+                next_test_X = next(test_iter)
+            
+            test_X = torch.cat((test_X, next_test_X), 0)
+            
+        # test_X = next(test_iter)
+        test_X = test_X.to(self.config.device)
+        if self.normalize:
+            test_X = running_mean_std(test_X)
+        plot_energy_curve(network, test_X)
+
+
+    def visualise_2d_energy(self):
+        """Visualise the energy function for 2D maze environment
+
+        An observation in the maze env is a 2D vector. The energy net is trained using s-s' pairs so the input is 4D. To visualise this in a 2D plane:
+        - First a meshgrid of the same size as the env is created
+        - For every point in the meshgrid, a window of the agent's reachable set is computed
+        - s-s' pairs are the ncomputed by pairing every meshgrid point with another point in the window (reachable set)
+        - The energy function is then computed in using this 4D input and the average energy is assigned to that meshgrid point
+        """
+        device = self.config.device
+        states = torch.load(self.config.inference.eb_model_checkpoint, map_location=self.config.device)
+        score = SimpleNet(self.config).to(self.config.device)
+        score = torch.nn.DataParallel(score)
+        score.load_state_dict(states[0])
+        score.eval()
+
+        colormask = self.config.visualise.colormask
+        viz_min = 0
+        viz_max = 512
+        c = self.config.inference.sigma_level # c ranges from [0,L-1]
+        kernel_size = 3 # must be odd
+        grid_steps = 128
+        window_idx_left = int((kernel_size - 1)/2)
+        window_idx_right = int((kernel_size + 1)/2)
+
+        if self.config.training.normalize_energynet_input:
+            running_mean_std = RunningMeanStd(torch.ones(self.config.model.in_dim * self.config.model.numObsSteps).shape).to(self.config.device)
+            running_mean_std_states = torch.load(self.config.inference.running_mean_std_checkpoint, map_location=self.config.device)
+            running_mean_std.load_state_dict(running_mean_std_states)
+            running_mean_std.eval()
+            
+            print(f"EnergyNet was trained using normalised inputs. Data mean {running_mean_std.running_mean} Data var {running_mean_std.running_var}")
+
+
+        sigmas = np.exp(np.linspace(np.log(self.config.model.sigma_begin), np.log(self.config.model.sigma_end),
+                                    self.config.model.L))
+        print(f"Sigma levels {[(i,val.item()) for i,val in enumerate(sigmas)]}")
+
+        xs = torch.linspace(viz_min, viz_max, steps=grid_steps)
+        ys = torch.linspace(viz_min, viz_max, steps=grid_steps)
+        x, y = torch.meshgrid(xs, ys, indexing='xy')
+
+        grid_points = torch.cat((x.flatten().view(-1, 1),y.flatten().view(-1,1)), 1).to(device=self.config.device)
+        grid_points = grid_points.reshape(grid_steps,grid_steps,2)
+        energy_grid = torch.zeros(grid_steps,grid_steps,1)
+
+        for i in range(grid_points.shape[0]):
+            for j in range(grid_points.shape[1]):
+                if i in [viz_min,viz_max] or j in [viz_min,viz_max]:
+                    pass
+                    
+                else:
+                    window = grid_points[i-window_idx_left:i+window_idx_right,j-window_idx_left:j+window_idx_right,:]
+                    grid_pt_window = torch.zeros_like(window)
+                    grid_pt_window[:,:,:] = grid_points[i,j]
+
+                    obs_pairs = torch.cat((window, grid_pt_window), 2)
+
+                    obs_pairs = obs_pairs.reshape(-1,4)
+                    labels = torch.ones(obs_pairs.shape[0], device=grid_points.device) * c # c ranges from [0,L-1]
+                    
+                    obs_pairs = running_mean_std(obs_pairs)
+                    energy = score(obs_pairs, labels)
+                    mean_energy = torch.mean(energy).item()
+                    energy_grid[i,j] = mean_energy
+
+        energy_grid = energy_grid.reshape(-1,x.shape[0])
+
+        if colormask:
+            plt.figure(figsize=(8, 6))
+            mesh = plt.pcolormesh(x.cpu().cpu().detach().numpy(), y.cpu().detach().numpy(), energy_grid.cpu().detach().numpy(), cmap ='gray')
+            plt.gca().invert_yaxis()
+            plt.xlabel("env - x")
+            plt.ylabel("env - y")
+            plt.title(f"Maze Env E(s,s' | c={c}) | Mean energy in agent's reachable set")
+            plt.colorbar(mesh)
+
+
+        plt.show()
+
+
 
 
     def visualise_sr_energy(self):
@@ -368,85 +520,3 @@ class AnnealRunner():
                 )
 
             plt.show()
-
-
-
-    def visualise_2d_energy(self):
-        """Visualise the energy function for 2D maze environment
-
-        An observation in the maze env is a 2D vector. The energy net is trained using s-s' pairs so the input is 4D. To visualise this in a 2D plane:
-        - First a meshgrid of the same size as the env is created
-        - For every point in the meshgrid, a window of the agent's reachable set is computed
-        - s-s' pairs are the ncomputed by pairing every meshgrid point with another point in the window (reachable set)
-        - The energy function is then computed in using this 4D input and the average energy is assigned to that meshgrid point
-        """
-        device = self.config.device
-        states = torch.load(self.config.inference.eb_model_checkpoint, map_location=self.config.device)
-        score = SimpleNet(self.config).to(self.config.device)
-        score = torch.nn.DataParallel(score)
-        score.load_state_dict(states[0])
-        score.eval()
-
-        colormask = self.config.visualise.colormask
-        viz_min = 0
-        viz_max = 512
-        c = self.config.inference.sigma_level # c ranges from [0,L-1]
-        kernel_size = 3 # must be odd
-        grid_steps = 128
-        window_idx_left = int((kernel_size - 1)/2)
-        window_idx_right = int((kernel_size + 1)/2)
-
-        if self.config.training.normalize_energynet_input:
-            running_mean_std = RunningMeanStd(torch.ones(self.config.model.in_dim * self.config.model.numObsSteps).shape).to(self.config.device)
-            running_mean_std_states = torch.load(self.config.inference.running_mean_std_checkpoint, map_location=self.config.device)
-            running_mean_std.load_state_dict(running_mean_std_states)
-            running_mean_std.eval()
-            
-            print(f"EnergyNet was trained using normalised inputs. Data mean {running_mean_std.running_mean} Data var {running_mean_std.running_var}")
-
-
-        sigmas = np.exp(np.linspace(np.log(self.config.model.sigma_begin), np.log(self.config.model.sigma_end),
-                                    self.config.model.L))
-        print(f"Sigma levels {[(i,val.item()) for i,val in enumerate(sigmas)]}")
-
-        xs = torch.linspace(viz_min, viz_max, steps=grid_steps)
-        ys = torch.linspace(viz_min, viz_max, steps=grid_steps)
-        x, y = torch.meshgrid(xs, ys, indexing='xy')
-
-        grid_points = torch.cat((x.flatten().view(-1, 1),y.flatten().view(-1,1)), 1).to(device=self.config.device)
-        grid_points = grid_points.reshape(grid_steps,grid_steps,2)
-        energy_grid = torch.zeros(grid_steps,grid_steps,1)
-
-        for i in range(grid_points.shape[0]):
-            for j in range(grid_points.shape[1]):
-                if i in [viz_min,viz_max] or j in [viz_min,viz_max]:
-                    pass
-                    
-                else:
-                    window = grid_points[i-window_idx_left:i+window_idx_right,j-window_idx_left:j+window_idx_right,:]
-                    grid_pt_window = torch.zeros_like(window)
-                    grid_pt_window[:,:,:] = grid_points[i,j]
-
-                    obs_pairs = torch.cat((window, grid_pt_window), 2)
-
-                    obs_pairs = obs_pairs.reshape(-1,4)
-                    labels = torch.ones(obs_pairs.shape[0], device=grid_points.device) * c # c ranges from [0,L-1]
-                    
-                    obs_pairs = running_mean_std(obs_pairs)
-                    energy = score(obs_pairs, labels)
-                    mean_energy = torch.mean(energy).item()
-                    energy_grid[i,j] = mean_energy
-
-        energy_grid = energy_grid.reshape(-1,x.shape[0])
-
-        if colormask:
-            plt.figure(figsize=(8, 6))
-            mesh = plt.pcolormesh(x.cpu().cpu().detach().numpy(), y.cpu().detach().numpy(), energy_grid.cpu().detach().numpy(), cmap ='gray')
-            plt.gca().invert_yaxis()
-            plt.xlabel("env - x")
-            plt.ylabel("env - y")
-            plt.title(f"Maze Env E(s,s' | c={c}) | Mean energy in agent's reachable set")
-            plt.colorbar(mesh)
-
-
-        plt.show()
