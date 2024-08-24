2a2af985c79212927e4e6b4251c63159ee6474f1
anishdiwan <anishdiwan@diwan-ubuntu20.04>
2024-08-24 15:12:46 +0200

    Add composed energy network

--- models/motion_scorenet.py
+++ models/motion_scorenet.py
@@ -3,6 +3,13 @@ import torch.nn.functional as F
 import torch
 from functools import partial
 import math
+from learning.motion_ncsn.models.ema import EMAHelper
+from isaacgymenvs.tasks.amp.humanoid_amp_base import NUM_OBS
+from isaacgymenvs.tasks.humanoid_amp import build_amp_observations
+from omegaconf import OmegaConf
+from isaacgymenvs.tasks.amp.utils_amp.motion_lib import MotionLib
+import os
+import numpy as np
 
 class SinusoidalPosEmb(nn.Module):
     def __init__(self, dim, steps=100):
@@ -85,7 +92,7 @@ class LatentSpaceTf(nn.Module):
         super().__init__()
         self.in_dim = in_dim
         self.out_dim = out_dim
-        self.layers = hidden_layers
+        self.layers = hidden_layers.copy()
 
         self.layers.insert(0, self.in_dim)
         self.layers.append(self.out_dim)
@@ -184,7 +191,6 @@ class SimpleNet(nn.Module):
     Args:
         in_dim (int): dimensionality of the input
         latent_space_dim (int): dimensionality of the latent space
-        cond_dim (int): dimensionality of the conditioning vector (cond)
         encoder_hidden_layers (list): list of the number of neurons in the hidden layers of the encoder (in order of the layers)
         decoder_hidden_layers (list): list of the number of neurons in the hidden layers of the decoder (in order of the layers)
     """
@@ -237,6 +243,160 @@ class SimpleNet(nn.Module):
             energy = self.decoder(out)
         
         return energy
+
+
+class ComposedEnergyNet():
+    def __init__(self, config, checkpoints, device, in_dim, use_ema, ema_rate=None, scale_energies=False, env=None):
+
+        self.energy_networks = []
+        self.config = config
+        self.energy_function_weights = [1/len(checkpoints)]*len(checkpoints)
+        self.motion_styles = list(checkpoints.keys())
+
+        for checkpoint in list(checkpoints.values()):
+            eb_model_states = torch.load(checkpoint, map_location=device)
+            energynet = SimpleNet(self.config, in_dim=in_dim).to(device)
+            energynet = torch.nn.DataParallel(energynet)
+            energynet.load_state_dict(eb_model_states[0])
+
+            if use_ema:
+                print("Using EMA model weights")
+                ema_helper = EMAHelper(mu=ema_rate)
+                ema_helper.register(energynet)
+                ema_helper.load_state_dict(eb_model_states[-1])
+                ema_helper.ema(energynet)
+            
+            energynet.eval()
+            self.energy_networks.append(energynet)
+
+
+        if scale_energies:
+            assert env is not None, "And env must be passed to compute scaling parameters"
+            self.init_motion_sampling(env, device)
+            self.energy_function_scaling = self.get_energy_function_scaling(device)
+            self.free_memory()
+        else:
+            self.energy_function_scaling = np.ones_like(self.energy_function_weights)
+
+    def __call__(self, x, cond, energy_function_weights=None):
+        """
+        Automatically compose learnt energy functions. Assumes that both energy functions have the same network structure
+
+        Args:
+            x (torch.Tensor): Input samples
+            cond: Noise level or the sigma value. Same as cond for SimpleNet.
+            energy_function_weights: Relative weightage of energy functions when combining their rewards
+        """
+        if energy_function_weights == None:
+            energy_function_weights = [torch.full((x.shape[0],1), weight, dtype=x.dtype, device=x.device) for weight in self.energy_function_weights]  #self.energy_function_weights
+        assert (energy_function_weights[0].shape[0] == x.shape[0]) and (len(energy_function_weights)==len(self.energy_networks)), "Please pass the same number of weights as there are composed energy functions"
+        output = None
+
+        for idx, energynet in enumerate(self.energy_networks):
+            if output == None:
+                output = energy_function_weights[idx]*self.energy_function_scaling[idx]*energynet(x, cond)
+            else:
+                output += energy_function_weights[idx]*self.energy_function_scaling[idx]*energynet(x, cond)
+
+        return output
+
+    def get_energy_function_scaling(self, device):
+        """Compute scaling factors to account for the scale difference between the learnt energy functions (energy functions are unnormalised)
+        """
+        num_samples = 4096
+        c=0
+        sigmas = self._get_ncsn_sigmas(device)
+        avg_energies = []
+        for motion_style, energynet in list(zip(self.motion_styles, self.energy_networks)):
+            motion_samples = self.sample_paired_traj(num_samples, motion_style)
+            labels = torch.ones(motion_samples.shape[0], device=device, dtype=torch.long) * c # c ranges from [0,L-1]
+            used_sigmas = sigmas[labels].view(motion_samples.shape[0], *([1] * len(motion_samples.shape[1:])))
+            perturbation_levels = {'labels':labels, 'used_sigmas':used_sigmas}
+            energies = energynet(motion_samples, perturbation_levels)
+            avg_energies.append(energies.squeeze().mean(dim=0))
+        
+        avg_energies = torch.tensor(avg_energies)
+        scaling = avg_energies[0]/avg_energies
+
+        return scaling
+ 
+
+    def init_motion_sampling(self, env, device):
+        """Initialise motion sampling to compute scaling factors
+
+        Args:
+            env (class): The environment class
+        """
+        motion_cfg = OmegaConf.create()
+        motion_cfg.env = self.config.data.env_params
+        motion_cfg.sim = self.config.data.sim_params
+        self.motion_cfg = motion_cfg
+        self._motion_libs = {}
+
+        for motion_style in self.motion_styles:
+
+            # First try to find motions in the main assets folder. Then try in the dataset directory
+            motion_style_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../../assets/amp/motions', motion_style)
+            if os.path.exists(motion_style_path):
+                pass
+
+            else:
+                motion_style_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../custom_envs/data/humanoid', motion_style)
+                assert os.path.exists(motion_style_path), "Provided motion file can not be found in the assets/amp/motions or data/humanoid directories"
+
+
+            self._motion_libs[motion_style] = MotionLib(motion_file=motion_style_path, 
+                                     num_dofs=env.humanoid_num_dof,
+                                     key_body_ids=env._key_body_ids.cpu().numpy(), 
+                                     device=device)
+
+    def free_memory(self):
+        self._motion_libs = None
+
+
+    def sample_paired_traj(self, num_samples, motion_style):
+        num_obs_steps = self.motion_cfg["env"].get("numObsSteps", 2)
+        dt = self.motion_cfg["sim"].get("dt", 0.0166)
+        out_shape = (num_samples, num_obs_steps, NUM_OBS)
+        num_obs = self.motion_cfg["env"].get("numObsSteps", 2) * NUM_OBS
+        motion_ids = self._motion_libs[motion_style].sample_motions(num_samples)
+        motion_times0 = self._motion_libs[motion_style].sample_time(motion_ids)
+
+        motion_ids = np.tile(np.expand_dims(motion_ids, axis=-1), [1, num_obs_steps])
+        motion_times = np.expand_dims(motion_times0, axis=-1)
+        
+
+        time_steps = -dt * np.arange(0, num_obs_steps)
+        motion_times = motion_times + time_steps
+
+        motion_ids = motion_ids.flatten()
+        motion_times = motion_times.flatten()
+        root_pos, root_rot, dof_pos, root_vel, root_ang_vel, dof_vel, key_pos \
+               = self._motion_libs[motion_style].get_motion_state(motion_ids, motion_times)
+        root_states = torch.cat([root_pos, root_rot, root_vel, root_ang_vel], dim=-1)
+        obs_demo = build_amp_observations(root_states, dof_pos, dof_vel, key_pos,
+                                      self.motion_cfg["env"]["localRootObs"])
+        obs_demo = obs_demo.view(out_shape)        
+        obs_demo_flat = obs_demo.view(-1, num_obs)
+        return obs_demo_flat
+        
+
+    def _get_ncsn_sigmas(self, device):
+        """Return the noise scales depending on the ncsn version
+        """
+        if self.config["model"]["ncsnv2"]:
+            # Geometric Schedule
+            sigmas = torch.tensor(
+                np.exp(np.linspace(np.log(self.config["model"]["sigma_begin"]), np.log(self.config["model"]["sigma_end"]),
+                                self.config["model"]["L"]))).float().to(device)
+        else:
+            # Uniform Schedule
+            sigmas = torch.tensor(
+                    np.linspace(self.config["model"]["sigma_begin"], self.config["model"]["sigma_end"], self.config["model"]["L"])
+                    ).float().to(device)
+
+        return sigmas
+
 
 
 
